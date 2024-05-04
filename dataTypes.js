347d8d6fa5f2c352d9a5be1b7d489e0d98758943
anas95062 <154525696+anas95062@users.noreply.github.com>
2024-05-04 22:33:48 +0530

    Add files via upload

---
+++ dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all js code as newer version
+
+// alert(3+3) // we are using nodejs, not browser
+
+console.log(typeof null)
+
+// DATA TYPES
+/*
+1. number => 2 to the power 53(range)
+2. bigint
+3. string => " "
+4. boolean => True/False
+5. null => standalon value
+6. undefined => like (let val; not assigned any number)
+7. symbol => used to find uniqueness of component.
+8. object
+*/
+
+let name = "Anas"
+let age = 23
+let isloggedin = true
+console.log(typeof undefined)
