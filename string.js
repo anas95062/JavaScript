2875b88fa64c1e60111aa7c949f760cbc24f8dfd
anas95062 <154525696+anas95062@users.noreply.github.com>
2024-05-05 15:38:58 +0530

    Add files via upload

---
+++ string.js
@@ -0,0 +1,21 @@
+const name = "Anas"
+const count = 80
+// console.log(name + count + "Thanks");
+
+console.log(`Hello My name is ${name} and my leet count is ${count}`);
+
+const gameName = new String('Subway surf')
+// console.log(gameName);
+// console.log(gameName[5]);
+
+// console.log(gameName.__proto__); // {} prototype
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(8));
+// console.log(gameName.indexOf('s'));
+// console.log(gameName.substring(0,4));
+console.log(gameName.slice(-8, 5));
+
+const str=new String('Motila Nehru National Institute of Technology')
+console.log(str);
+const str1 = str.split(' ')
+console.log(str1);
